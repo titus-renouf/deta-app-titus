62b6b44a1ff1dc9f20d545c077a269f4807f0cba
titus.renouf <titus.renouf@bluescale.com>
2022-12-04 19:33:19 +0100

    launch GA

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-def app(event):
-    return "Hello, world!"
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def hello_world():
+    prefix_google = """
+    <!-- Google tag (gtag.js) -->
+    <script async
+    src="https://www.googletagmanager.com/gtag/js?id=G-E8P9VNLNRR"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', ' G-E8P9VNLNRR ');
+    </script>
+    """
+    return prefix_google + "Hello World"
